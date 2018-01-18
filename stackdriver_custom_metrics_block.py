f8322ee4131f0a28ffd2bf158aad7a8af320d455
Chris Brackert <cbrackert@gmail.com>
2018-01-18 11:05:35 -0700

    Add service credentials config

--- stackdriver_custom_metrics_block.py
+++ stackdriver_custom_metrics_block.py
@@ -1,6 +1,8 @@
 from google.cloud import monitoring
+from google.oauth2 import service_account
 from nio.block.base import Block
 from nio.properties import VersionProperty, FloatProperty, StringProperty
+# google.auth.exceptions.DefaultCredentialsError
 
 
 class StackdriverCustomMetrics(Block):
@@ -9,10 +11,17 @@ class StackdriverCustomMetrics(Block):
     value = FloatProperty(title='Value', default='{{ $value }}')
     metric_type = StringProperty(
         title='Metric Type', default='custom.googleapis.com/metric')
+    service_account_file = StringProperty(
+        title='Service Account File',
+        default='[[PROJECT_ROOT]]/project-abcd1234.json',
+    )
 
     def process_signals(self, signals):
-        client = monitoring.Client()
+        client = monitoring.Client.from_service_account_json(
+            json_credentials_path=self.service_account_file())
         metric = client.metric(type_=self.metric_type(), labels={})
         resource = client.resource('global', labels={})
         for signal in signals:
+            self.logger.debug('Write {} to {}'.format(
+                self.value(signal), self.metric_type()))
             client.write_point(metric, resource, self.value(signal))
--- tests/test_stackdriver_custom_metrics_block.py
+++ tests/test_stackdriver_custom_metrics_block.py
@@ -11,30 +11,44 @@ class TestStackdriverCustomMetrics(NIOBlockTestCase):
         config = config or {}
         block = StackdriverCustomMetrics()
         metric_type = config.get('metric_type', 'custom.googleapis.com/metric')
+        default_filename = '[[PROJECT_ROOT]]/project-abcd1234.json'
+        filename = config.get('service_account_file', default_filename)
         with patch(block.__module__ + '.monitoring') as monitoring:
-            client = MagicMock()
-            metric = MagicMock()
-            client.metric.return_value = metric
-            resource = MagicMock()
-            client.resource.return_value = resource
-            monitoring.Client.return_value = client
-            self.configure_block(block, config)
-            block.start()
-            block.process_signals(signals)
-            block.stop()
+            with patch(block.__module__ + '.service_account') as sa:
+                client = MagicMock()
+                metric = MagicMock()
+                client.metric.return_value = metric
+                resource = MagicMock()
+                client.resource.return_value = resource
+                monitoring.Client.from_service_account_json.return_value = \
+                    client
+                self.configure_block(block, config)
+                block.start()
+                block.process_signals(signals)
+                block.stop()
         self.assert_num_signals_notified(0)
+        monitoring.Client.from_service_account_json.assert_called_once_with(
+            json_credentials_path=filename)
         client.metric.assert_called_once_with(type_=metric_type, labels={})
         client.resource.assert_called_once_with('global', labels={})
         return client, metric, resource
 
-    def test_process_signals_default_config(self):
-        """Each signal calls write_point"""
+    def test_default_config(self):
+        """Write one data point with default config"""
         signals = [Signal({'value': 3.14})]
         client, metric, resource = self._test(signals)
         client.write_point.assert_called_once_with(metric, resource, 3.14)
 
-    def test_process_signals(self):
-        """Each signal calls write_point"""
+    def test_service_account_file_config(self):
+        """Service Account File location is configurable"""
+        config = {
+            'service_account_file': 'file_location.json',
+        }
+        signals = [Signal({'value': 0.99})]
+        client, metric, resource = self._test(signals, config)
+
+    def test_value_and_metric_type_config(self):
+        """Metric Type and value propety are configurable"""
         config = {
             'value': '{{ $float }}',
             'metric_type': 'some_metric',
@@ -43,8 +57,8 @@ class TestStackdriverCustomMetrics(NIOBlockTestCase):
         client, metric, resource = self._test(signals, config)
         client.write_point.assert_called_once_with(metric, resource, 0.99)
 
-    def test_process_signals_list(self):
-        """Each signal calls write_point"""
+    def test_signal_list(self):
+        """Write a data point for each signal"""
         signals = [
             Signal({'value': 0.99}),
             Signal({'value': 3.14}),
